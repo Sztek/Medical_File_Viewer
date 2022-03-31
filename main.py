b680fbc4a671057b6c21bf69070c99193ff5a75b
Sztek <jakubrogusz@gmail.com>
2022-03-31 14:35:22 +0200

    menu

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from tkinter import *
 from pydicom import *
-import glob
+import glob, os
 # from pydicom.data import get_testdata_file
 from pydicom.fileset import FileSet
 from PIL import Image, ImageTk
+from math import sqrt
+
+
+def load(name):
+    fs = FileSet()
+    if os.path.isdir(name):
+        for file in glob.glob(name+'/*.dcm'):
+            ds = dcmread(file)
+            fs.add(ds)
+    return fs.find()
 
 
 class Plotno:
-    warstwa = 0
-    canvas = 0
-    instance = 0
-    img = 0
+    def __init__(self, root):
+        self.warstwa = 0
+        self.size = 512
+        self.canvas = Canvas
+        self.instance = FileSet
+        self.img = ImageTk
+        self.canvas = Canvas(root, width=520, height=512)
+
+        self.canvas.bind('<MouseWheel>', self.tick)
 
     def tick(self, event):
         self.warstwa += int(event.delta/(-120))
@@ -21,11 +36,13 @@ class Plotno:
         # print(self.warstwa)
         self.draw()
 
-    def create(self, root, instance):
-        self.canvas = Canvas(root, width=520, height=512)
-        self.canvas.pack()
-        self.canvas.bind('<MouseWheel>', self.tick)
-        self.instance = instance
+    def set(self, instance):
+        if instance:
+            self.instance = instance
+            self.warstwa = 0
+            self.size = (sqrt(instance[0].load().pixel_array.size))
+            self.canvas.pack()
+            self.draw()
 
     def draw(self):
         self.img = ImageTk.PhotoImage(image=Image.fromarray(self.instance[self.warstwa].load().pixel_array))
@@ -34,17 +51,19 @@ class Plotno:
         self.canvas.create_rectangle(512, self.warstwa*502/200, 520, self.warstwa*502/200+10, fill='black', outline='')
 
 
-pole = Plotno()
 root = Tk()
+pole = Plotno(root)
 
-fs = FileSet()
-for file in glob.glob('pliki/*.dcm'):
-    ds = dcmread(file)
-    fs.add(ds)
-# print(fs)
-instance = fs.find()
+menu = Frame(root)
+menu.pack()
 
-pole.create(root, instance)
-pole.draw()
+title = Label(menu, text='DICOM Viewer', font=('Arial', 20))
+title.pack()
+
+name = Entry(menu)
+name.pack()
+
+button = Button(menu, text='load', command=lambda: pole.set(load(name.get())))
+button.pack()
 
 root.mainloop()
