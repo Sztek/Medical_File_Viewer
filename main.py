7bc683a23114b6a731291b3c1027e96a34406d88
Sztek <jakubrogusz@gmail.com>
2022-05-26 16:15:29 +0200

    nifti działa

--- main.py
+++ main.py
@@ -5,17 +5,7 @@ import nibabel as nib
 import glob
 import os
 from PIL import Image, ImageTk
-from math import sqrt
 from numpy import swapaxes
-
-
-def load(path):
-    pixels = []
-    if os.path.isdir(path):
-        for file in glob.glob(path + '/*.dcm'):
-            ds = dcmread(file)
-            pixels.append(ds.pixel_array)
-    return pixels
 
 
 class Plotno:
@@ -23,7 +13,6 @@ class Plotno:
         self.warstwa = 0
         self.warstwa_s = 0
         self.warstwa_b = 0
-        self.size = 512
         self.pixels = []
         self.pixels_s = []
         self.pixels_b = []
@@ -48,45 +37,45 @@ class Plotno:
             wys = len(self.pixels) + len(self.pixels_s) + 8
             self.canvas = Canvas(root, width=sze, height=wys)
             self.warstwa = 0
-            self.size = (sqrt(len(pixels[0])-1))
             self.canvas.pack(side='right', padx=10, pady=10)
             self.draw()
-            self.canvas.bind('<MouseWheel>', self.scroll_tick)
             self.canvas.bind('<Motion>', self.move_tick)
             self.canvas.bind('<Button-1>', self.onclick)
 
-    def scroll_tick(self, event):
-        self.warstwa += int(event.delta/(-120))
-        if self.warstwa > len(self.pixels)-1:
-            self.warstwa = len(self.pixels)-1
-        if self.warstwa < 0:
-            self.warstwa = 0
-        self.draw()
-
     def move_tick(self, event):
+        x = self.canvas.winfo_pointerx() - self.canvas.winfo_rootx()
+        y = self.canvas.winfo_pointery() - self.canvas.winfo_rooty()
         if not self.clicked:
-            self.warstwa_s = (self.canvas.winfo_pointerx() - self.canvas.winfo_rootx())
-            if self.warstwa_s >= 512:
-                self.warstwa_s = 511
-            self.warstwa_b = (self.canvas.winfo_pointery() - self.canvas.winfo_rooty())
-            if self.warstwa_b >= 512:
-                self.warstwa_b = 511
+            if x < len(self.pixels_s):
+                self.warstwa_s = x
+                if self.warstwa_s >= len(self.pixels_s):
+                    self.warstwa_s = len(self.pixels_s)-1
+            elif len(self.pixels_s) + 8 < x < len(self.pixels_s) + 8 + len(self.pixels):
+                self.warstwa = x - len(self.pixels_b) - 8
+            if y < len(self.pixels_b):
+                self.warstwa_b = y
+                if self.warstwa_b >= len(self.pixels_b):
+                    self.warstwa_b = len(self.pixels_b)-1
+            elif len(self.pixels_b) + 8 < y < len(self.pixels_b) + 8 + len(self.pixels):
+                self.warstwa = y - len(self.pixels_s) - 8
         self.draw()
 
     def draw(self):
+        red = len(self.pixels) + len(self.pixels_b) + 8
+        blue = len(self.pixels) + len(self.pixels_s) + 8
         self.canvas.delete('all')
-        self.img = ImageTk.PhotoImage(image=Image.fromarray(self.pixels[self.warstwa], mode=self.display))
-        self.img_s = ImageTk.PhotoImage(image=Image.fromarray(self.pixels_s[self.warstwa_s], mode=self.display))
-        self.img_b = ImageTk.PhotoImage(image=Image.fromarray(self.pixels_b[self.warstwa_b], mode=self.display))
+        self.img = ImageTk.PhotoImage(image=Image.fromarray(self.pixels[self.warstwa]))
+        self.img_s = ImageTk.PhotoImage(image=Image.fromarray(self.pixels_s[self.warstwa_s]))
+        self.img_b = ImageTk.PhotoImage(image=Image.fromarray(self.pixels_b[self.warstwa_b]))
         if not self.hidden:
-            self.canvas.create_rectangle(0, 520-1, 511, 520+200, outline='blue')
-            self.canvas.create_rectangle(520-1, 0, 520+200, 511, outline='red')
+            self.canvas.create_rectangle(0, len(self.pixels_b)+7, len(self.pixels_s)-1, red, outline='blue')
+            self.canvas.create_rectangle(len(self.pixels_s)+7, 0, blue, len(self.pixels_b)-1, outline='red')
         self.canvas.create_image(0, 0, anchor="nw", image=self.img)
-        self.canvas.create_image(520, 0, anchor="nw", image=self.img_s)
-        self.canvas.create_image(0, 520, anchor="nw", image=self.img_b)
+        self.canvas.create_image(len(self.pixels_s)+8, 0, anchor="nw", image=self.img_s)
+        self.canvas.create_image(0, len(self.pixels_b)+8, anchor="nw", image=self.img_b)
         if not self.hidden:
-            self.canvas.create_rectangle(self.warstwa_s, 0, self.warstwa_s+1, 512, fill='red', outline='')
-            self.canvas.create_rectangle(0, self.warstwa_b, 512, self.warstwa_b+1, fill='blue', outline='')
+            self.canvas.create_rectangle(self.warstwa_s, 0, self.warstwa_s + 1, red, fill='red', outline='')
+            self.canvas.create_rectangle(0, self.warstwa_b, blue, self.warstwa_b + 1, fill='blue', outline='')
 
     def onclick(self, event):
         if self.clicked:
@@ -109,6 +98,20 @@ class Plotno:
             self.display = "I;16"
         self.draw()
 
+    def loadDicom(self, path):
+        pixels = []
+        if os.path.isdir(path):
+            for file in glob.glob(path + '/*.dcm'):
+                ds = dcmread(file)
+                pixels.append(ds.pixel_array)
+        return pixels
+
+    def loadNifti(self, path):
+        name = os.path.join(path.name)
+        img = nib.load(name)
+        pixels = img.get_fdata()
+        return pixels
+
 
 root = Tk()
 root.state('zoomed')
@@ -122,19 +125,16 @@ menu.pack(side='left', padx=20, pady=20)
 title = Label(menu, text='DICOM Viewer', font=('Arial', 20))
 title.pack()
 
-button = Button(menu, text='Select Directory', command=lambda: pole.set(load(fd.askdirectory())))
-button.pack()
+buttond = Button(menu, text='Select Dicom', command=lambda: pole.set(pole.loadDicom(fd.askdirectory())))
+buttond.pack()
+
+buttonn = Button(menu, text='Select Nifti', command=lambda: pole.set(pole.loadNifti(fd.askopenfile())))
+buttonn.pack()
+
 hide = Button(menu, text='hide axis', command=pole.hide)
 hide.pack()
 change = Button(menu, text='change display mode', command=pole.change)
 change.pack()
-
-nname = os.path.join('NN_06.nii.gz')
-img = nib.load(nname)
-x=img.get_fdata()
-nimg = ImageTk.PhotoImage(image=Image.fromarray(x[100]))
-can = Canvas(root,width=1000,height=1000)
-can.create_image(100,100,anchor="nw",image=nimg)
 
 
 root.mainloop()
